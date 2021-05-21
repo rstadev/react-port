877f3414cb51f20b5a08f0ef32d4c621ec1587ca
unknown <nukasine@gmail.com>
2021-05-21 11:36:36 -0600

    added a footer at the bottom of the page and styled it. This footer is sticky and appears on multiple pages.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavTabs from "./components/NavTabs";
 import Home from "./components/pages/Homepage/Home";
 import Portfolio from "./components/pages/Portfoliopage/Portfolio";
 import Contact from "./components/pages/Contactpage/Contact";
+import Footer from "./components/Footer";
 // import Project from "./components/pages/Project";
 // import backgroundimg from "./garden.jpg"
 
@@ -16,6 +17,7 @@ function App() {
         <Route exact path="/portfolio" component={Portfolio} />
         {/* <Route exact path="/project" component={Project} /> */}
         <Route exact path="/contact" component={Contact} />
+        <Footer />
     </Router>
       // </div>
   );
