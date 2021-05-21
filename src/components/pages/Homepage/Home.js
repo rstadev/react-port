3c52fdb1e388e75a580cafe5091a71ed9292a31a
unknown <nukasine@gmail.com>
2021-05-21 09:38:03 -0600

    added code from my portfolio and got it rendered on the home page

--- src/components/pages/Homepage/Home.js
+++ src/components/pages/Homepage/Home.js
@@ -1,22 +1,23 @@
 import React from "react";
+import "./style.css";
 
 function Home() {
 
   
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque velit, lobortis ut magna
-        varius, blandit rhoncus sem. Morbi lacinia nisi ac dui fermentum, sed luctus urna tincidunt.
-        Etiam ut feugiat ex. Cras non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna
-        imperdiet ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras rutrum
-        ligula in tincidunt commodo. Morbi sit amet mollis orci, in tristique ex. Donec nec ornare
-        elit. Donec blandit est sed risus feugiat porttitor. Vestibulum molestie hendrerit massa non
-        consequat. Vestibulum vitae lorem tortor. In elementum ultricies tempus. Interdum et
-        malesuada fames ac ante ipsum primis in faucibus.
-      </p>
-    </div>
+    // <div>
+    //   <h1>Home Page</h1>
+    //   <p>
+    //     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque velit, lobortis ut magna
+    //     varius, blandit rhoncus sem. Morbi lacinia nisi ac dui fermentum, sed luctus urna tincidunt.
+    //     Etiam ut feugiat ex. Cras non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna
+    //     imperdiet ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras rutrum
+    //     ligula in tincidunt commodo. Morbi sit amet mollis orci, in tristique ex. Donec nec ornare
+    //     elit. Donec blandit est sed risus feugiat porttitor. Vestibulum molestie hendrerit massa non
+    //     consequat. Vestibulum vitae lorem tortor. In elementum ultricies tempus. Interdum et
+    //     malesuada fames ac ante ipsum primis in faucibus.
+    //   </p>
+    // </div>
 //     <head>
 //     <meta charset="UTF-8">
 //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -44,44 +45,25 @@ function Home() {
 //             </div>
 //         </div>
 //     </nav>
-//     <section class="d-flex justify-content-center">
-//         <div id="homeSection">
-//             <div class = "text-center">
-//             <h1>About Me</h1>
-//             <hr/>
-//         </div>
-//             <img id="bio-image" class="rounded mx-auto d-block" src="./assets/misc/File_000.jpeg" alt="bio-picture">
-//             <hr />
-//             <p class="floatedtext"> Hi! My name is Conner Saxton and this is my Homepage! I am currently a student of
-//                 Chemistry at Georgia State University and enrolled in the Full Stack Development course with the Georgia
-//                 Institute of Technology. This portfolio was created as a part of a project in the Full Stack Development
-//                 course. <br><br />Please, click on <a class="dark underline" href="contact.html">Contact Me</a> in
-//                 either the navBar or in this paragraph to get into contact with me on one of my accounts.
-//         </div>
-
-//         <!-- Future code. Would like to retool the navbar so that Contact Me is a div under my bio in the home page, and another feature is put into the navbar. -->
-//         <!----------------------------------------------------------------------------->
-//         <!-- <div id='contactMe'>
-
-//         </div>
-//         </p>
-//         </div> -->
-//         <!----------------------------------------------------------------------------->
-//     </section>
+<div id="homeSection">
+  <div class = "text-center">
+    <h1>About Me</h1>
+    <hr/>
+  </div>
+  {/* <img id="bio-image" class="rounded mx-auto d-block" src="./assets/misc/File_000.jpeg" alt="bio-picture"> */}
+    <hr/>
+  <p class="floatedtext"> Hi! My name is Conner Saxton and this is my Homepage! I am currently a student of
+    Chemistry at Georgia State University and enrolled in the Full Stack Development course with the Georgia
+    Institute of Technology. This portfolio was created as a part of a project in the Full Stack Development
+    course. <br></br>Please, click on <a class="dark underline" href="contact.html">Contact Me</a> in
+    either the navBar or in this paragraph to get into contact with me on one of my accounts.
+  </p>
+</div>
 
 
 
-
-//     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-//         integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-//         crossorigin="anonymous"></script>
-//     <script src="https://code.jquery.com/jquery-3.5.1.js"
-//         integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
-//     <script src='./assets/script.js'></script>
-
-// </body>
   );
-}
+};
 
 
 
